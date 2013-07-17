60d5e9e497c7b52446c5ff0651bd676fa2f5704f
unknown <grazioli@HP650-RGRA.office.mailup.it>
2013-07-17 11:32:32 +0200

    -

--- Sample 2/html_mail.php
+++ Sample 2/html_mail.php
@@ -1,8 +1,8 @@
 <?php
  include_once ('html_functions.php');
  
- $to ="sijanzafeer@gmail.com"; // to address 
- $from= "s20341_17@in.smtpok.com"; // from address
+ $to =""; // to address 
+ $from= ""; // from address
  $from_name= "Last Name"; // name of the sender
  $subject= "HTML TEST"; // Subject
  $body= "contents.html"; // the html page nae
