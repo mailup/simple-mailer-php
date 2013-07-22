4a9bc81f47eba8b4a5e32990d4644d742f1f9817
unknown <grazioli@HP650-RGRA.office.mailup.it>
2013-07-22 10:34:02 +0200

    -

--- Sample 2/index.php
+++ Sample 2/index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        print "hello world";
-        ?>
-    </body>
-</html>
+<?php
+ include_once ('html_functions.php');
+ 
+ $to =""; // to address 
+ $from= ""; // from address
+ $from_name= "Last Name"; // name of the sender
+ $subject= "HTML TEST"; // Subject
+ $body= "contents.html"; // the html page nae
+ $attachement_1 = "phpmailer_mini.gif";
+ $attachement_2 = "phpmailer.gif";
+ 
+ if (smtpmailer($to, $from, $from_name, $subject, $body, $attachement_1, $attachement_2)) {
+	echo "Email sent.....<br/>";
+  }
+	
+?>
